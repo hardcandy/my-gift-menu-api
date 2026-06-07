b63f61965018baf4d27301ffd7d6cb88af6c2376
wuenzhen <wuenzhen@sinotrans.com>
2026-06-07 08:49:21 +0800

    Add client share config

--- src/main/java/com/wx/gift/controller/HomeController.java
+++ src/main/java/com/wx/gift/controller/HomeController.java
@@ -1,10 +1,13 @@
 package com.wx.gift.controller;
 
+import com.wx.gift.dto.ClientConfigDTO;
 import com.wx.gift.model.BaseUser;
 import com.wx.gift.service.UserService;
 import com.wx.gift.vo.LoginVo;
 import com.wx.gift.vo.ProfileVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +18,9 @@ public class HomeController {
     @Autowired
     private UserService userService;
 
+    @Value("${client.miniProgramPathPrefix:}")
+    private String miniProgramPathPrefix;
+
     @RequestMapping("/userInfoByCode")
     public BaseUser getUserInfo(@RequestBody LoginVo vo) {
         return userService.login(vo);
@@ -24,5 +30,13 @@ public class HomeController {
     public BaseUser updateProfile(@RequestBody ProfileVo vo) {
         return userService.updateProfile(vo);
     }
-}
 
+    @RequestMapping("/clientConfig")
+    public ClientConfigDTO clientConfig() {
+        String prefix = StringUtils.trimWhitespace(miniProgramPathPrefix);
+        return ClientConfigDTO.builder()
+                .shareEnabled(StringUtils.hasText(prefix))
+                .miniProgramPathPrefix(prefix)
+                .build();
+    }
+}
---
+++ src/main/java/com/wx/gift/dto/ClientConfigDTO.java
@@ -0,0 +1,17 @@
+package com.wx.gift.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@Accessors(chain = true)
+public class ClientConfigDTO {
+    private boolean shareEnabled;
+    private String miniProgramPathPrefix;
+}
